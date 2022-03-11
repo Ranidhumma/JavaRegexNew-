b33e5831ec2f644489cbcf51024e58937bf1f19b
Ranidhumma <ranidhumma8@gmail.com>
2022-03-11 22:14:00 +0530

    lastnameCaps

--- src/com/java/RegexUserReg.java
+++ src/com/java/RegexUserReg.java
@@ -4,25 +4,34 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class RegexUserReg {
+	static Scanner scanner = new Scanner(System.in);
 
 	/* First name starts with Cap and has minimum 3 characters */
 	public static void FirstName() {
 		System.out.println("Enter First Name");
-		Scanner scanner = new Scanner(System.in);
 		String fN = scanner.nextLine();
-
 		if (Pattern.matches("[A-Z]{1}[a-z]{2,}", fN) == true)
 			System.out.println("Valid Input passed");
 		else
 			System.out.println("Invalid input passed");
+	}
 
-		scanner.close();
+	/* Last name starts with Cap and has minimum 3 characters */
+	public static void LastName() {
+		System.out.println("Enter Last Name");
+		String lN = scanner.nextLine();
+		if (Pattern.matches("[a-z]{2,}[A-Z]{1}", lN) == true)
+			System.out.println("Valid Input passed");
+		else
+			System.out.println("Invalid input passed");
+
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("Welcome to user registartion");
-		FirstName();
+		// FirstName();
+		LastName();
 	}
 }
