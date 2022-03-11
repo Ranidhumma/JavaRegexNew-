541efe0452aab5f193006ea22dc4b1a65b762b03
Ranidhumma <ranidhumma8@gmail.com>
2022-03-11 21:46:16 +0530

    welcome to user registation

---
+++ src/com/java/RegexUserReg.java
@@ -0,0 +1,11 @@
+package com.java;
+
+public class RegexUserReg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to user registartion");
+
+	}
+}
